810eaec9f23c37d89ed53440c7c82262e95d8488
micksuffolk <82178824+micksuffolk@users.noreply.github.com>
2021-05-23 21:01:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,27 +7,19 @@ from time import sleep
 
 
 ######################################################################################################################
-# Initialize variables...
-ControlCommandWord1 = int()
-ControlCommandWord2 = int()
-ControlCommandWord3 = int()
-ControlCommandWord4 = int()
-ControlDataWord1 = int()
-ControlDataWord2 = int()
-ControlDataWord3 = int()
-ControlDataWord4 = int()
-ControlDataWord5 = int()
-ControlDataWord6 = int()
-ControlDataWord7 = int()
-ControlDataWord8 = int()
+# Initialize some variables which need to be at the start of the code...
+ControlDataWord = list()
 ExecuteControlModbusWrite = bool()
+ExecuteControlModbusDataWrite = bool()
+HR_Write_Address_BitSet = int()
+HR_Write_Value_BitSet = int()
 
 
 
 ######################################################################################################################
 # Function to check if a bit in an integer is ON or OFF...
 # Integer range is 0 to 65535, bit range is 0 to 15...
-def BitCheck(Integer_to_BitCheck, BitToCheck):
+def WordRead_BitCheck(Integer_to_BitCheck, BitToCheck):
     if (Integer_to_BitCheck & (1 << BitToCheck)):
         return True # Bit is Logic 1 (one)
     else:
@@ -36,62 +28,60 @@ def BitCheck(Integer_to_BitCheck, BitToCheck):
 
 
 ######################################################################################################################
-# Function to set a bit in an integer ON...
-# CtrlWord range is 1 to 4, bit range is 0 to 15...
-def BitSet(CtrlWord, bit):
+# Function to set a bit in a Holding Register ON (Modbus HR Write, from button press later on)...
+# bit range is 0 to 15...
+def WordWrite_BitSet(HR_Address, bit):
 
-    global ControlCommandWord1
-    global ControlCommandWord2
-    global ControlCommandWord3
-    global ControlCommandWord4
     global ExecuteControlModbusWrite
+    global HR_Write_Address_BitSet
+    global HR_Write_Value_BitSet
 
-    temp_output = int()
+    HR_Write_Address_BitSet = int(HR_Address)
 
     if bit == 0:
-        temp_output = int(0) | int(1)
+        HR_Write_Value_BitSet = int(1)
     if bit == 1:
-        temp_output = int(0) | int(2)
+        HR_Write_Value_BitSet = int(2)
     if bit == 2:
-        temp_output = int(0) | int(4)
+        HR_Write_Value_BitSet = int(4)
     if bit == 3:
-        temp_output = int(0) | int(8)
+        HR_Write_Value_BitSet = int(8)
     if bit == 4:
-        temp_output = int(0) | int(16)
+        HR_Write_Value_BitSet = int(16)
     if bit == 5:
-        temp_output = int(0) | int(32)
+        HR_Write_Value_BitSet = int(32)
     if bit == 6:
-        temp_output = int(0) | int(64)
+        HR_Write_Value_BitSet = int(64)
     if bit == 7:
-        temp_output = int(0) | int(128)
+        HR_Write_Value_BitSet = int(128)
     if bit == 8:
-        temp_output = int(0) | int(256)
+        HR_Write_Value_BitSet = int(256)
     if bit == 9:
-        temp_output = int(0) | int(512)
+        HR_Write_Value_BitSet = int(512)
     if bit == 10:
-        temp_output = int(0) | int(1024)
+        HR_Write_Value_BitSet = int(1024)
     if bit == 11:
-        temp_output = int(0) | int(2048)
+        HR_Write_Value_BitSet = int(2048)
     if bit == 12:
-        temp_output = int(0) | int(4096)
+        HR_Write_Value_BitSet = int(4096)
     if bit == 13:
-        temp_output = int(0) | int(8192)
+        HR_Write_Value_BitSet = int(8192)
     if bit == 14:
-        temp_output = int(0) | int(16384)
+        HR_Write_Value_BitSet = int(16384)
     if bit == 15:
-        temp_output = int(0) | int(-32768)
+        HR_Write_Value_BitSet = int(-32768)
 
-    if CtrlWord == 1:
-        ControlCommandWord1 = temp_output
-    if CtrlWord == 2:
-        ControlCommandWord2 = temp_output
-    if CtrlWord == 3:
-        ControlCommandWord3 = temp_output
-    if CtrlWord == 4:
-        ControlCommandWord4 = temp_output
-
-    temp_output = 0
     ExecuteControlModbusWrite = True
+
+
+
+######################################################################################################################
+# Function to data write to HR (from button press later on)...
+def WordWrite_TriggerDataWrite(trigger):
+
+    global ExecuteControlModbusDataWrite
+
+    ExecuteControlModbusDataWrite = True
 
 
 
@@ -146,6 +136,15 @@ try:
     Label_Rasp_Pi_StatusDataWord14  = StringVar()
     Label_Rasp_Pi_StatusDataWord15  = StringVar()
     Label_Rasp_Pi_StatusDataWord16  = StringVar()
+
+    Entry_01_Rasp_Pi   = IntVar()
+    Entry_02_Rasp_Pi   = IntVar()
+    Entry_03_Rasp_Pi   = IntVar()
+    Entry_04_Rasp_Pi   = IntVar()
+    Entry_05_Rasp_Pi   = IntVar()
+    Entry_06_Rasp_Pi   = IntVar()
+    Entry_07_Rasp_Pi   = IntVar()
+    Entry_08_Rasp_Pi   = IntVar()
 
     Header_01 = Label(root, text="---------------------------- Modbus TCP Client (Master) - Live Raspberry Pi Modbus Data ----------------------------", bg='grey', fg='black')
     Header_01.grid(row=0, columnspan=99, sticky=EW)
@@ -213,37 +212,42 @@ try:
 
     Button_CtrlWord_Text_01  = Label(root, bg='grey', text="Ctrl Word #1").grid(row=13, column=0, sticky=E)
 
-    Button_CtrlWord_Bit_00 = Button(root, text="Bit 0", fg="black", bg='grey', height=1, width=7, command=lambda: BitSet(1, 0)).grid(row=13, column=2)
-    Button_CtrlWord_Bit_01 = Button(root, text="Bit 1", fg="black", bg='grey', height=1, width=7, command=lambda: BitSet(1, 1)).grid(row=13, column=3)
-    Button_CtrlWord_Bit_02 = Button(root, text="Bit 2", fg="black", bg='grey', height=1, width=7, command=lambda: BitSet(1, 2)).grid(row=13, column=4)
-    Button_CtrlWord_Bit_03 = Button(root, text="Bit 3", fg="black", bg='grey', height=1, width=7, command=lambda: BitSet(1, 3)).grid(row=13, column=5)
-    Button_CtrlWord_Bit_04 = Button(root, text="Bit 4", fg="black", bg='grey', height=1, width=7, command=lambda: BitSet(1, 4)).grid(row=13, column=6)
-    Button_CtrlWord_Bit_05 = Button(root, text="Bit 5", fg="black", bg='grey', height=1, width=7, command=lambda: BitSet(1, 5)).grid(row=13, column=7)
-    Button_CtrlWord_Bit_06 = Button(root, text="Bit 6", fg="black", bg='grey', height=1, width=7, command=lambda: BitSet(1, 6)).grid(row=13, column=8)
-    Button_CtrlWord_Bit_07 = Button(root, text="Bit 7", fg="black", bg='grey', height=1, width=7, command=lambda: BitSet(1, 7)).grid(row=13, column=9)
+    Button_CtrlWord_Bit_00 = Button(root, text="Bit 0", fg="black", bg='grey', height=1, width=7, command=lambda: WordWrite_BitSet(1, 0)).grid(row=13, column=2)
+    Button_CtrlWord_Bit_01 = Button(root, text="Bit 1", fg="black", bg='grey', height=1, width=7, command=lambda: WordWrite_BitSet(1, 1)).grid(row=13, column=3)
+    Button_CtrlWord_Bit_02 = Button(root, text="Bit 2", fg="black", bg='grey', height=1, width=7, command=lambda: WordWrite_BitSet(1, 2)).grid(row=13, column=4)
+    Button_CtrlWord_Bit_03 = Button(root, text="Bit 3", fg="black", bg='grey', height=1, width=7, command=lambda: WordWrite_BitSet(1, 3)).grid(row=13, column=5)
+    Button_CtrlWord_Bit_04 = Button(root, text="Bit 4", fg="black", bg='grey', height=1, width=7, command=lambda: WordWrite_BitSet(1, 4)).grid(row=13, column=6)
+    Button_CtrlWord_Bit_05 = Button(root, text="Bit 5", fg="black", bg='grey', height=1, width=7, command=lambda: WordWrite_BitSet(1, 5)).grid(row=13, column=7)
+    Button_CtrlWord_Bit_06 = Button(root, text="Bit 6", fg="black", bg='grey', height=1, width=7, command=lambda: WordWrite_BitSet(1, 6)).grid(row=13, column=8)
+    Button_CtrlWord_Bit_07 = Button(root, text="Bit 7", fg="black", bg='grey', height=1, width=7, command=lambda: WordWrite_BitSet(1, 7)).grid(row=13, column=9)
 
-    Button_CtrlWord_Bit_08 = Button(root, text="Bit 8", fg="black", bg='grey', height=1, width=7, command=lambda: BitSet(1, 8)).grid(row=14, column=2)
-    Button_CtrlWord_Bit_09 = Button(root, text="Bit 9", fg="black", bg='grey', height=1, width=7, command=lambda: BitSet(1, 9)).grid(row=14, column=3)
-    Button_CtrlWord_Bit_10 = Button(root, text="Bit 10", fg="black", bg='grey', height=1, width=7, command=lambda: BitSet(1, 10)).grid(row=14, column=4)
-    Button_CtrlWord_Bit_11 = Button(root, text="Bit 11", fg="black", bg='grey', height=1, width=7, command=lambda: BitSet(1, 11)).grid(row=14, column=5)
-    Button_CtrlWord_Bit_12 = Button(root, text="Bit 12", fg="black", bg='grey', height=1, width=7, command=lambda: BitSet(1, 12)).grid(row=14, column=6)
-    Button_CtrlWord_Bit_13 = Button(root, text="Bit 13", fg="black", bg='grey', height=1, width=7, command=lambda: BitSet(1, 13)).grid(row=14, column=7)
-    Button_CtrlWord_Bit_14 = Button(root, text="Bit 14", fg="black", bg='grey', height=1, width=7, command=lambda: BitSet(1, 14)).grid(row=14, column=8)
-    Button_CtrlWord_Bit_15 = Button(root, text="Bit 15", fg="black", bg='grey', height=1, width=7, command=lambda: BitSet(1, 15)).grid(row=14, column=9)
+    Button_CtrlWord_Bit_08 = Button(root, text="Bit 8", fg="black", bg='grey', height=1, width=7, command=lambda: WordWrite_BitSet(1, 8)).grid(row=14, column=2)
+    Button_CtrlWord_Bit_09 = Button(root, text="Bit 9", fg="black", bg='grey', height=1, width=7, command=lambda: WordWrite_BitSet(1, 9)).grid(row=14, column=3)
+    Button_CtrlWord_Bit_10 = Button(root, text="Bit 10", fg="black", bg='grey', height=1, width=7, command=lambda: WordWrite_BitSet(1, 10)).grid(row=14, column=4)
+    Button_CtrlWord_Bit_11 = Button(root, text="Bit 11", fg="black", bg='grey', height=1, width=7, command=lambda: WordWrite_BitSet(1, 11)).grid(row=14, column=5)
+    Button_CtrlWord_Bit_12 = Button(root, text="Bit 12", fg="black", bg='grey', height=1, width=7, command=lambda: WordWrite_BitSet(1, 12)).grid(row=14, column=6)
+    Button_CtrlWord_Bit_13 = Button(root, text="Bit 13", fg="black", bg='grey', height=1, width=7, command=lambda: WordWrite_BitSet(1, 13)).grid(row=14, column=7)
+    Button_CtrlWord_Bit_14 = Button(root, text="Bit 14", fg="black", bg='grey', height=1, width=7, command=lambda: WordWrite_BitSet(1, 14)).grid(row=14, column=8)
+    Button_CtrlWord_Bit_15 = Button(root, text="Bit 15", fg="black", bg='grey', height=1, width=7, command=lambda: WordWrite_BitSet(1, 15)).grid(row=14, column=9)
 
-    Entry_01_Spacer = Label(root, text="", bg='grey', fg='black')
-    Entry_01_Spacer.grid(row=15, columnspan=99, sticky=EW)
+    Entry_Spacer = Label(root, text="", bg='grey', fg='black')
+    Entry_Spacer.grid(row=15, columnspan=99, sticky=EW)
 
     Entry_Text  = Label(root, bg='grey', text="Control Data").grid(row=16, column=0, sticky=E)
 
-    Entry_01 = Entry(root, width=7).grid(row=16, column=2)
-    Entry_02 = Entry(root, width=7).grid(row=16, column=3)
-    Entry_03 = Entry(root, width=7).grid(row=16, column=4)
-    Entry_04 = Entry(root, width=7).grid(row=16, column=5)
-    Entry_05 = Entry(root, width=7).grid(row=16, column=6)
-    Entry_06 = Entry(root, width=7).grid(row=16, column=7)
-    Entry_07 = Entry(root, width=7).grid(row=16, column=8)
-    Entry_08 = Entry(root, width=7).grid(row=16, column=9)
+    Entry_01 = Entry(root, width=7, textvariable=Entry_01_Rasp_Pi).grid(row=16, column=2)
+    Entry_02 = Entry(root, width=7, textvariable=Entry_02_Rasp_Pi).grid(row=16, column=3)
+    Entry_03 = Entry(root, width=7, textvariable=Entry_03_Rasp_Pi).grid(row=16, column=4)
+    Entry_04 = Entry(root, width=7, textvariable=Entry_04_Rasp_Pi).grid(row=16, column=5)
+    Entry_05 = Entry(root, width=7, textvariable=Entry_05_Rasp_Pi).grid(row=16, column=6)
+    Entry_06 = Entry(root, width=7, textvariable=Entry_06_Rasp_Pi).grid(row=16, column=7)
+    Entry_07 = Entry(root, width=7, textvariable=Entry_07_Rasp_Pi).grid(row=16, column=8)
+    Entry_08 = Entry(root, width=7, textvariable=Entry_08_Rasp_Pi).grid(row=16, column=9)
+
+    Button_Entry_Spacer = Label(root, text="", bg='grey', fg='black')
+    Button_Entry_Spacer.grid(row=17, columnspan=99, sticky=EW)
+
+    Button_Entry_Accept = Button(root, text="Send Data", fg="black", bg='grey', height=1, width=7, command=lambda: WordWrite_TriggerDataWrite(1)).grid(row=18, column=2)
 
     Footer_01_Spacer = Label(root, text="", bg='grey', fg='black')
     Footer_01_Spacer.grid(row=98, columnspan=99, sticky=EW)
@@ -271,37 +275,47 @@ else:
     # Main Program Loop...
     while True:
 
-        Write_Data_HR = list()
-        Write_Data_HR.append(ControlCommandWord1)
-        Write_Data_HR.append(ControlCommandWord2)
-        Write_Data_HR.append(ControlCommandWord3)
-        Write_Data_HR.append(ControlCommandWord4)
-        Write_Data_HR.append(ControlDataWord1)
-        Write_Data_HR.append(ControlDataWord2)
-        Write_Data_HR.append(ControlDataWord3)
-        Write_Data_HR.append(ControlDataWord4)
-        Write_Data_HR.append(ControlDataWord5)
-        Write_Data_HR.append(ControlDataWord6)
-        Write_Data_HR.append(ControlDataWord7)
-        Write_Data_HR.append(ControlDataWord8)
-
-        write_HR_message_01 = tcp.write_multiple_registers(slave_id=1, starting_address=0, values=[1])
-        write_HR_message_02 = tcp.write_multiple_registers(slave_id=1, starting_address=1, values=Write_Data_HR)
-        read_HR_message_01 = tcp.read_holding_registers(slave_id=1, starting_address=100, quantity=7)
-        read_IR_message_01 = tcp.read_input_registers(slave_id=1, starting_address=0, quantity=18)
-
-        # Response depends on Modbus function code. This particular returns the
-        # amount of coils written, in this case it is.
-        write_HR_message_01_response = tcp.send_message(write_HR_message_01, sock)
+        # Modbus Write control words to Pi (only on button press)...
+        write_HR_message_BitSet = tcp.write_multiple_registers(slave_id=1, starting_address=int(HR_Write_Address_BitSet), values=[HR_Write_Value_BitSet])
         if ExecuteControlModbusWrite:
-            write_HR_message_02_response = tcp.send_message(write_HR_message_02, sock)
+            write_HR_message_BitSet_response = tcp.send_message(write_HR_message_BitSet, sock)
             ExecuteControlModbusWrite = False
-        read_HR_message_01_response = tcp.send_message(read_HR_message_01, sock)
-        read_IR_message_01_response = tcp.send_message(read_IR_message_01, sock)
 
-        for x in read_HR_message_01_response:
+
+        # Modbus Write heartbeat to Pi (continuously)...
+        write_HR_message_Heartbeat = tcp.write_multiple_registers(slave_id=1, starting_address=0, values=[1])
+        write_HR_message_Heartbeat_response = tcp.send_message(write_HR_message_Heartbeat, sock)
+
+
+        # Modbus Write Control Data Words to Pi (continuously)...
+        ControlDataWord.append(Entry_01_Rasp_Pi.get())
+        ControlDataWord.append(Entry_02_Rasp_Pi.get())
+        ControlDataWord.append(Entry_03_Rasp_Pi.get())
+        ControlDataWord.append(Entry_04_Rasp_Pi.get())
+        ControlDataWord.append(Entry_05_Rasp_Pi.get())
+        ControlDataWord.append(Entry_06_Rasp_Pi.get())
+        ControlDataWord.append(Entry_07_Rasp_Pi.get())
+        ControlDataWord.append(Entry_08_Rasp_Pi.get())
+        write_HR_message_ControlDataWord = tcp.write_multiple_registers(slave_id=1, starting_address=5, values=ControlDataWord)
+        if ExecuteControlModbusDataWrite:
+            write_HR_message_ControlDataWord_response = tcp.send_message(write_HR_message_ControlDataWord, sock)
+            ExecuteControlModbusDataWrite = False
+        ControlDataWord.clear()
+
+        # Modbus Read Pi Time/Date (continuously)...
+        read_HR_message_Time = tcp.read_holding_registers(slave_id=1, starting_address=100, quantity=7)
+        read_HR_message_Time_response = tcp.send_message(read_HR_message_Time, sock)
+
+
+        # Modbus Read Pi general data stores (continuously)...
+        read_IR_message_data = tcp.read_input_registers(slave_id=1, starting_address=0, quantity=18)
+        read_IR_message_data_response = tcp.send_message(read_IR_message_data, sock)
+
+
+        # Data from HR read message, move to "Received_Data" list...
+        for x in read_HR_message_Time_response:
             Received_Data_HR.append(x)
-
+        # Move data from list to relevant program variables...
         Label_Rasp_Pi_Hour.set(str(Received_Data_HR[0]))
         Label_Rasp_Pi_Minute.set(str(Received_Data_HR[1]))
         Label_Rasp_Pi_Second.set(str(Received_Data_HR[2]))
@@ -309,10 +323,14 @@ else:
         Label_Rasp_Pi_Year.set(str(Received_Data_HR[4]))
         Label_Rasp_Pi_Month.set(str(Received_Data_HR[5]))
         Label_Rasp_Pi_Day.set(str(Received_Data_HR[6]))
+        # Clear the temporary data store ready for the next cycle of Modbus reads...
+        Received_Data_HR.clear()
 
-        for x in read_IR_message_01_response:
+
+        # Data from IR read message, move to "Received_Data" list...
+        for x in read_IR_message_data_response:
             Received_Data_IR.append(x)
-
+        # Move data from list to relevant program variables...
         Label_Rasp_Pi_ProgramStatus.set(str(Received_Data_IR[0]))
         Label_Rasp_Pi_Temp.set(str(Received_Data_IR[1]))
         Label_Rasp_Pi_StatusDataWord1.set(str(Received_Data_IR[2]))
@@ -331,21 +349,13 @@ else:
         Label_Rasp_Pi_StatusDataWord14.set(str(Received_Data_IR[15]))
         Label_Rasp_Pi_StatusDataWord15.set(str(Received_Data_IR[16]))
         Label_Rasp_Pi_StatusDataWord16.set(str(Received_Data_IR[17]))
-
-        # Check if a bit in an integer is ON or OFF... (testing only)
-        #x = BitCheck(65535, 15)
-        #print(x)
-
-        #print(Received_Data_HR)
-
-        Received_Data_HR.clear()
+        # Clear the temporary data store ready for the next cycle of Modbus reads...
         Received_Data_IR.clear()
-        Write_Data_HR.clear()
 
+
+        # Update the GUI...
         root.update_idletasks()
         root.update()
-
-        sleep(0.1)
 
 
 
@@ -361,7 +371,3 @@ finally:
     exit()
 
 
-
-######################################################################################################################
-#def CheckBoxCmd(event):
-#    root.update()
